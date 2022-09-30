2ad0938f53c42b86b2dfe0d2dbe90c434b44db0a
Christina Bohn <christinajbohn@gmail.com>
2022-09-29 19:58:04 -0700

    Adding file structure for pages, routes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 
+// Import Pages & Components
+import Home from './pages/Home';
+import Bio from './pages/Bio';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+
+
 function App() {
+
+  const pages = ['Home', 'Bio', 'Projects', 'Contact'];
+  const [currentPage, setCurrentPage] = useState(pages[0])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+          <div className="App">
+
+            {/* Header Navigation Bar */}
+            <ResponsiveAppBar
+            pages={pages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage} />
+
+            {/* Pages */}
+            <Routes>
+              <Route exact path = '/Home' element={<Home/>} />
+              <Route exact path = '/Bio' element={<Bio/>} />
+              <Route exact path = '/Projects' element={<Projects/>} />
+              <Route exact path = '/Contact' element={<Contact/>} />
+            </Routes>
+
+            {/* Footer Icons */}
+            <Footer/>
+
+          </div>
+        </Router>
   );
 }
 
